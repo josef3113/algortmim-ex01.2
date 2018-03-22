b6c3cb22a690deb72fdbec529b8f3dc14dbc87ec
josef3113 <33261454+josef3113@users.noreply.github.com>
2018-03-22 17:38:45 +0200

    avg

--- algortmim ex01.2/Statits.cpp
+++ algortmim ex01.2/Statits.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <time.h>
 using namespace std;
 
 int MinMax(int* i_Arr, int i_size)
@@ -9,42 +10,88 @@ int MinMax(int* i_Arr, int i_size)
     
 	for (int i = 0; i < i_size; i++)
 	{
+		countComp++;
 		if (i_Arr[i] > max)
 		{
 			max = i_Arr[i];
-			countComp++;
+			
 		}
 		else
 		{
+			countComp++;
 			if (i_Arr[i] < min)
 			{
 				min = i_Arr[i];
-				countComp++;
+				
 			}
 		}
 	}
 	return countComp;
 }
 
-
-int main()
+int* MixArr(int* Arr, int i_size)
 {
-	int sumOfComp = 0;
-	int numOfArrys = 10000, sizeOfArry = 1000;
-	
-	int* arr = new int [sizeOfArry] ;
 
-	for (int i = 0; i < numOfArrys; i++)
+	for (int i = 0; i < i_size; i++)
 	{
-		for (int j = 0; j < sizeOfArry; j++)
+		swap(Arr[i], Arr[rand() % 1000]);
+	}
+	return Arr;
+}
+
+int staticsOfCompRand(int i_numOfArry, int i_sizeOfArr)
+{
+
+	srand(time(0));
+	int sumOfComp = 0;
+	int* arr = new int[i_sizeOfArr];
+
+	for (int i = 0; i < i_numOfArry; i++)
+	{
+		for (int j = 0; j < i_sizeOfArr; j++)
 		{
-			arr[j] = rand() % sizeOfArry;
+			arr[j] = (rand() % i_sizeOfArr) + 1;
 		}
 
-		sumOfComp += MinMax(arr, sizeOfArry);
+		sumOfComp += MinMax(arr, i_sizeOfArr);
+
+	}
+	return sumOfComp / i_numOfArry;
+}
+int statisOfCmpShafelArry(int i_timeToShafell, int i_sizeOfArry)
+{
+	srand(time(0));
+	int sumOfComp = 0;
+	int* arr = new int[i_sizeOfArry];
+	for (int j = 0; j < i_sizeOfArry; j++)
+	{
+		arr[j] = (rand() % i_sizeOfArry) + 1;
+	}
+
+	for (int i = 0; i < i_timeToShafell; i++)
+	{
+
+		arr = MixArr(arr, i_sizeOfArry);
+		sumOfComp += MinMax(arr, i_sizeOfArry);
 
 	}
 
-	cout << " the avg of compers is :" << sumOfComp / numOfArrys << endl;
+	return sumOfComp / i_timeToShafell;
+
+}
+int main()
+{
+	int numOfArrys = 100000, sizeOfArry = 1000;
+
+	int avgOfCompDiffrentArrys = staticsOfCompRand(numOfArrys, sizeOfArry);
+	int avgOfCompShaffelArry = statisOfCmpShafelArry(numOfArrys, sizeOfArry);
+
+	
+
+	
+	
+
+	cout <<"the avg of compers in diffrent arreys is :" << avgOfCompDiffrentArrys<< endl;
+	cout << "the avg of comper in shafel arry is : " << avgOfCompShaffelArry << endl;
 
 }
